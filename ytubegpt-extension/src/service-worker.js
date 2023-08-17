2fd19739e6216ffbf7ee2da03641d6886c1b3bf4
Jalal Uddin <dgvai.hridoy@gmail.com>
2023-08-17 23:53:01 +0600

    fix: register ta update listener independently

--- ytubegpt-extension/src/service-worker.js
+++ ytubegpt-extension/src/service-worker.js
@@ -1,11 +1,9 @@
-chrome.runtime.onInstalled.addListener(function () {
-  chrome.tabs.onUpdated.addListener(async function (tabId, changeInfo, tab) {
-    if (tab.url.includes("youtube.com")) {
-      if (changeInfo.url) {
-        await chrome.tabs.sendMessage(tabId, { urlChanged: true });
-      }
+chrome.tabs.onUpdated.addListener(async function (tabId, changeInfo, tab) {
+  if (tab.url.includes("youtube.com")) {
+    if (changeInfo.url) {
+      await chrome.tabs.sendMessage(tabId, { urlChanged: true });
     }
-  });
+  }
 });
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
